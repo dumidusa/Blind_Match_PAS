e23d2e070ee5c6a6f9fe150e082cd3f7d0a8bc3a
dumidusa <fortermux298@gmail.com>
2026-04-15 10:11:25 +0530

    created the user model page!

---
+++ Models/user.cs
@@ -0,0 +1,18 @@
+namespace BlindMatchPAS.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string FullName { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+        public string PasswordHash { get; set; } = string.Empty;
+
+        public string Role { get; set; } = "Student"; 
+        
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
